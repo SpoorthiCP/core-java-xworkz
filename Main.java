1dd50c4bef159f3d1237720a1be31c98eeb50db7
SpoorthiCP <spooshetty1@gmail.com>
2023-07-20 09:29:34 +0530

    using for each

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main
+{
+static int age[]={13,12,5,16};
+
+public static void main(String hlo[]){
+/*for(int reference:age){
+System.out.println(reference);
+
+}*/
+for(int i=0;i<age.length;i++){
+	System.out.println(age[i]);
+}
+}
+}
