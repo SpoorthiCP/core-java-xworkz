f6588437b5c5781231e3bf66f1ca72edb2ee0bfc
SpoorthiCP <spooshetty1@gmail.com>
2023-07-24 22:26:46 +0530

    its an simple eg of encapsulation

---
+++ Customer.java
@@ -0,0 +1,31 @@
+class Customer{
+ private int customerId;
+ private String firstName;
+private String lastName;
+String address;
+String dob;
+ long phoneNo;
+
+ //getter and setter
+ public void setCustomerId(int customerId){
+	 this.customerId=customerId;
+ }
+ public int getCustomerId(){
+	 return customerId;
+ }
+
+ public void setName(String firstName){	
+this.firstName=firstName;
+}
+ public String getName(){
+	return firstName; 
+ }
+
+public void setNamess(String lastName){	
+this.lastName=lastName;
+}
+ public String getNamess(){
+	return lastName; 
+ }
+ 
+ }
---
+++ CustomerTester.java
@@ -0,0 +1,19 @@
+class CustomerTester{
+public static void main(String hii[]){
+
+Customer details=new Customer();
+//details.customerId=1;
+//details.firstName="bus";
+details.setCustomerId(1);
+System.out.println(details.getCustomerId());
+
+details.setName("SPOORTHI");
+System.out.println(details.getName());
+
+details.setNamess("SHETTY");
+System.out.println(details.getNamess());
+
+//details.setLast("shetty");
+//System.out.println(details.getLast());
+}
+}
